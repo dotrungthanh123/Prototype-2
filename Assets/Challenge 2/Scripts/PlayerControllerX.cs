3891a47aa7ce5de68ac5a3efa32da9f596611853
ThanhDT <dotrungthanh2300@gmail.com>
2024-06-19 16:15:19 +0700

    Unit 2
    Collision detection + random

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    
+    private bool ready;
+
+    private void Start() {
+        ready = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && ready)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            ready = false;
+            Invoke(nameof(Ready), .5f);
+        }
+    }
+
+    private void Ready() {
+        ready = true;
+    }
+}
